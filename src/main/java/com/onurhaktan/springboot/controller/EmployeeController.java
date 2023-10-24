42e78c25dd7b583e53f5c3ae7a012ec0438cd12e
Onur Haktan <onurhaktan243@gmail.com>
2023-10-24 23:20:20 +0300

    build createEmployee endpoint

---
+++ src/main/java/com/onurhaktan/springboot/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.onurhaktan.springboot.controller;
+
+import com.onurhaktan.springboot.model.Employee;
+import com.onurhaktan.springboot.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/employees")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeService.saveEmployee(employee);
+    }
+}
