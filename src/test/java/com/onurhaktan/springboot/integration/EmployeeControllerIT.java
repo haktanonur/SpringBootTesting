0c9d9971375ca8968334d0a24ee3a174951e161a
Onur Haktan <onurhaktan243@gmail.com>
2023-10-31 23:19:24 +0300

    add DynamicPropertySource annotation

--- src/test/java/com/onurhaktan/springboot/integration/EmployeeControllerIT.java
+++ src/test/java/com/onurhaktan/springboot/integration/EmployeeControllerIT.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.testcontainers.containers.MySQLContainer;
@@ -32,6 +34,12 @@ public class EmployeeControllerIT {
     @Container // manage the lifecycle of this container
     private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
 
+    @DynamicPropertySource
+    public static void dynamicPropertySource(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
     @Autowired
     private MockMvc mockMvc;
 
